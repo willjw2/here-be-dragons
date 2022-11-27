48867b8c52fd64407dbf561117ff5e4afecfd21b
Will Wang <willy.j.wang@gmail.com>
2022-11-27 03:17:47 -0600

    Get tests to pass

--- spec/nytimes_spec.rb
+++ spec/nytimes_spec.rb
@@ -3,39 +3,50 @@ require 'pry'
 require './lib/nytimes'
 
 class Nytimes
-  describe 'data' do 
-    before :each do 
+  describe 'data' do
+    before :each do
       @hash = Nytimes::DATA
-    end 
-  
-    it 'can get status' do 
+    end
+
+    it 'can get status' do
       #FINISHED EXAMPLE: Using @hash, define a variable called `result` that returns the status
       result = @hash[:status]
-      
+
       expect(result).to eq("OK")
     end
 
-    xit 'can get copyright' do 
+    it 'can get copyright' do
       #Using @hash, define a variable called `result` that returns the copyright
+      result = @hash[:copyright]
 
       expect(result).to eq("Copyright (c) 2018 The New York Times Company. All Rights Reserved.")
     end
 
-    xit 'can get array of stories' do 
+    it 'can get array of stories' do
       #Using @hash, define a variable called `result` that returns the array of stories
-  
+      result = @hash[:results]
+
       expect(result).to be_an_instance_of(Array)
       expect(result.count).to eq(44)
     end
 
-    xit 'can get all stories with subsection of politics' do 
+    it 'can get all stories with subsection of politics' do
       #Using @hash, define a variable called `result` that returns all stories with subsection of politics.
-  
+      results = @hash[:results]
+      result = Array.new
+      results.each do |story|
+        if story[:subsection] == "Politics"
+          result << story
+        end
+      end
+
+      # require "pry"; binding.pry
+
       expect(result).to be_an_instance_of(Array)
       expect(result.count).to eq(6)
       expect(result.first[:title]).to eq("Congressional G.O.P. Agenda Quietly Falls Into Place Even as Trump Steals the Spotlight")
       expect(result.last[:title]).to eq("Conspiracy Theories Made Alex Jones Very Rich. They May Bring Him Down.")
     end
-  end 
+  end
 
 end
